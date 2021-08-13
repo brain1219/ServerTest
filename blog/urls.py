198a68e6c7ae8d063f0cb3caf6160b61cc34934a
M.J.,Son <sonbill971@gmail.com>
2021-08-14 00:44:10 +0900

    Respons 부분 완성

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog import views
+from .views import cat_and_dog_pred_api
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -7,4 +8,5 @@ urlpatterns = [
     path("post/create", views.PostCreate.as_view(), name="post_create"),
     path("cat_dog", views.catOrdog.as_view(), name="cat_dog"),
     path("cat_dog/result", views.catOrdog_result, name="catOrdog_result"),
+    path("catdogapi/",cat_and_dog_pred_api),
 ]
--- myblog/urls.py
+++ myblog/urls.py
@@ -19,11 +19,21 @@ from django.conf.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from blog import views
+from django.conf.urls import url, include
+
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostViewset)
+router.register(r'cat_and_dogs', views.CatAndDogViewset)
+# router.register(r'catdogapi', views.CatAndDogUploadApiViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("blog/", include("blog.urls")),  # blog/ 를 입력하면 blog 앱이 처리함
     path("", RedirectView.as_view(url="/blog/", permanent=True)), #기본 주소 입력시 blog로 넘겨줌
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
